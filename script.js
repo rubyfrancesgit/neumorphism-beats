68332cf130b5ea97b80cbbd9d3edfadc127d0aa4
Ruby Doyle <270159356@bun2-206-05.acg.ac.nz>
2021-11-29 14:49:05 +1300

    colours

--- script.js
+++ script.js
@@ -22,22 +22,34 @@ $(document).ready(function(){
 
 
         function padsDown(key){
+            let colours = ['#FDE5B9','#B5EFE0','#B0D9F3','#D8B4E2','#FF9BC5','#FF9798'];
+            let randomColour = colours[Math.floor(Math.random() * colours.length)];
+            console.log(randomColour);
+
             const beatPad = document.querySelectorAll('.pad');
 
             if(key.keyCode == '49'){
                 beatPad[0].style.boxShadow="inset 2px 2px 10px 0 rgba(0, 0, 0, .15), -2px -2px 10px 0 rgba(255, 255, 255, 1)";
+
+                $('.1').css('background-color', randomColour)
             }
 
             if(key.keyCode == '50'){
                 beatPad[1].style.boxShadow="inset 2px 2px 10px 0 rgba(0, 0, 0, .15), -2px -2px 10px 0 rgba(255, 255, 255, 1)";
+                
+                $('.2').css('background-color', randomColour)
             }
 
             if(key.keyCode == '51'){
                 beatPad[2].style.boxShadow="inset 2px 2px 10px 0 rgba(0, 0, 0, .15), -2px -2px 10px 0 rgba(255, 255, 255, 1)";
+                
+                $('.3').css('background-color', randomColour)
             }
 
             if(key.keyCode == '52'){
                 beatPad[3].style.boxShadow="inset 2px 2px 10px 0 rgba(0, 0, 0, .15), -2px -2px 10px 0 rgba(255, 255, 255, 1)";
+                
+                $('.4').css('background-color', randomColour)
             }
         }
 
@@ -50,6 +62,8 @@ $(document).ready(function(){
                 let audio = new Audio('./sounds/snare-808.wav');
 
                 audio.play();
+
+                $('.1').css('background-color', '#f9f9f9');
             }
 
             if(key.keyCode == '50'){
@@ -58,6 +72,8 @@ $(document).ready(function(){
                 let audio = new Audio('./sounds/kick-heavy.wav');
 
                 audio.play();
+
+                $('.2').css('background-color', '#f9f9f9');
             }
 
             if(key.keyCode == '51'){
@@ -66,6 +82,8 @@ $(document).ready(function(){
                 let audio = new Audio('./sounds/hihat-808.wav');
 
                 audio.play();
+
+                $('.3').css('background-color', '#f9f9f9');
             }
 
             if(key.keyCode == '52'){
@@ -74,6 +92,8 @@ $(document).ready(function(){
                 let audio = new Audio('./sounds/hihat-acoustic01.wav');
 
                 audio.play();
+
+                $('.4').css('background-color', '#f9f9f9');
             }
         }
 
